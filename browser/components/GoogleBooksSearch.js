36909d28a02d2b7216491955b5de99bae2274cdb
Elliott Brooks <ms.elliottbrooks@gmail.com>
2017-05-10 12:28:22 -0400

    google books search works

--- browser/components/GoogleBooksSearch.js
+++ browser/components/GoogleBooksSearch.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { GOOGLE_BOOKS_API } from '../../secrets.js';
 import { setGoogleBooks } from '../reducers/googleBooksReducer';
-import { setGoogleBook} from '../reducers/singleGoogleBookReducer';
+import { setGoogleBook, removeGoogleBook } from '../reducers/singleGoogleBookReducer';
 import { connect } from 'react-redux';
+import DatePicker from 'react-bootstrap-date-picker';
+import { addBook } from '../reducers/booksReducer';
+import { browserHistory } from 'react-router';
+
 
 const getBookInfo = (bookObj) => {
-	console.log(bookObj);
 	let key = bookObj.id;
 	let bookInfo = bookObj.volumeInfo;
 	let author = bookInfo.authors ? bookInfo.authors.join(', ') : 'Unknown';
-	let imageURL = bookInfo.imageLinks ? bookInfo.imageLinks.thumbnail : 'http://www.conservativefreepress.com/wp-content/uploads/2016/06/black-book-thumbnail.png';
+	let imageURL = bookInfo.imageLinks ? bookInfo.imageLinks.thumbnail : `https://www.mobileread.com/forums/attachment.php?attachmentid=11128${Math.floor(Math.random() * 4) + 1}&d=1378756884`;
 	let title = bookInfo.title ? bookInfo.title : 'Unknown';
 	return {key, author, imageURL, title};
 };
@@ -18,9 +21,11 @@ const getBookInfo = (bookObj) => {
 class GoogleBooksSearch extends Component {
     constructor (props) {
         super(props);
-        this.state = {query: ''};
+        this.state = {query: '', date: null };
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.searchSubmit = this.searchSubmit.bind(this);
+        this.finalSubmit = this.finalSubmit.bind(this);
+        this.handleDateChange = this.handleDateChange.bind(this);
     }
 
 	handleChange(event) {
@@ -28,26 +33,49 @@ class GoogleBooksSearch extends Component {
 		this.setState( {query: event.target.value} );
 	}
 
-	handleSubmit(event) {
+	handleDateChange(value) {
+        this.setState({
+            date: value
+        });
+    }
+
+	searchSubmit(event) {
 		event.preventDefault();
 		if (!this.state.query) return;
 		let query = this.state.query.split(' ').join('%20');
 		axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&orderBy=relevance&key=${GOOGLE_BOOKS_API}`)
 		.then(res => res.data.items)
 		.then(books => books.map(book => getBookInfo(book)))
-		.then(bookInfo => this.props.setBooks(bookInfo))
+		.then(bookInfo => this.props.setBooks(bookInfo));
 	}
 
-	handleClick(book, event) {
+
+	handleClick(book) {
 		this.props.setSingleBook(book);
 	}
+
+	finalSubmit(event) {
+        event.preventDefault();
+        var bookToAdd = this.props.selectedGoogleBook;
+        var title = bookToAdd.title;
+        var author = bookToAdd.author;
+        var url = bookToAdd.imageURL;
+        var notes = event.target.notes.value;
+        var date = this.state.date;
+        var starred = event.target.starred.value;
+        var reqObj = { title, author, url, notes, date, starred };
+        this.props.handleAddBook(reqObj);
+        this.props.setBooks([]);
+        this.props.removeBook();
+        browserHistory.push('/addedbooksuccess');
+    }
 
 	render() {
 			return (
 				<div>
 					<div className="jumbotron">
-						<h1>Find Book:</h1>
-						<form onSubmit={this.handleSubmit} >
+						<h1>Find a book:</h1>
+						<form onSubmit={this.searchSubmit} >
 					        <div className="form-group">
 					            <input type="text" className="form-control" placeholder="Search" onChange={this.handleChange} value={this.state.query} />
 					        </div>
@@ -57,9 +85,10 @@ class GoogleBooksSearch extends Component {
 				    {this.props.books.length
 				    ? <div>
 						<h1>Select a book:</h1>
+						<hr />
 						{ this.props.books.map( book =>
 							<ul key={book.key} onClick={this.handleClick.bind(this, book)}>
-								<img src={book.imageURL} />
+								<img src={book.imageURL} height="200" />
 									<li className="title">{book.title}</li>
 									<li className="author">{book.author}</li>
 							</ul>
@@ -67,24 +96,64 @@ class GoogleBooksSearch extends Component {
 					</div>
 					: null
 				}
-				{Object.keys(this.props.selectedBook).length
-					? <h1>Add Notes for <i>{this.props.selectedBook.title}</i>:</h1>
-
-					: null }
-
+				{Object.keys(this.props.selectedGoogleBook).length
+					?
+					<div>
+						<h1>Add notes to <i>{this.props.selectedGoogleBook.title}:</i></h1>
+						<hr />
+						<form className="add-book-form" onSubmit={this.finalSubmit}>
+							<div className="form-group">
+								<label htmlFor="notes" className="col-sm-2 control-label">Notes *</label>
+								<div className="col-sm-10">
+									<textarea name="notes" rows="5" type="text" className="form-control" />
+								</div>
+							</div>
+		                    <div className="form-group">
+		                        <label htmlFor="date" className="col-sm-2 control-label">Date Read</label>
+		                        <div className="col-sm-10">
+		                            <DatePicker onChange={this.handleDateChange} value={this.state.date} />
+		                        </div>
+		                    </div>
+							<div className="form-group">
+		                        <label htmlFor="starred" className="col-sm-2 control-label">Favorited</label>
+		                        <div className="col-sm-10">
+		                            <select name="starred">
+		                                <option>no</option>
+		                                <option>yes</option>
+		                            </select>
+		                        </div>
+							</div>
+							<p className="side-note">* Bookshelf supports notes written in Markdown!</p>
+		                    <div className="col-sm-offset-10 col-sm-10">
+		                        <button type="submit" className="btn btn-primary btn-lg">Submit</button>
+		                    </div>
+						</form>
+					</div>
+					: null
+				}
 			    </div>
 			);
 		}
 }
 
+const addBookToDB = (book) => {
+	return (dispatch) => {
+		axios.post(`/api/books`, book)
+		.then((res) => res.data)
+		.then((newBook) => dispatch(addBook(newBook)));
+	};
+};
+
 function mapStateToProps(state) {
-    return { books: state.googleBooks, selectedBook: state.selectedGoogleBook };
+    return { books: state.googleBooks, selectedGoogleBook: state.selectedGoogleBook };
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return {
 		setBooks: (books) => dispatch(setGoogleBooks(books)),
-		setSingleBook: (book) => dispatch(setGoogleBook(book))
+		setSingleBook: (book) => dispatch(setGoogleBook(book)),
+		removeBook: () => dispatch(removeGoogleBook()),
+		handleAddBook: (book) => dispatch(addBookToDB(book))
 	};
 };
 
--- browser/reducers/singleGoogleBookReducer.js
+++ browser/reducers/singleGoogleBookReducer.js
@@ -1,13 +1,20 @@
 /* ============ CONSTANTS =============== */
 
-const SET_BOOK = 'SET_BOOK';
+const SET_GOOGLE_BOOK = 'SET_GOOGLE_BOOK';
+const REMOVE_GOOGLE_BOOK = 'REMOVE_GOOGLE_BOOK';
 
 /* ============ ACTION CREATORS =============== */
 
 export const setGoogleBook = function(book) {
     return {
-        type: SET_BOOK,
+        type: SET_GOOGLE_BOOK,
         book: book
+    };
+};
+
+export const removeGoogleBook = function() {
+    return {
+        type: REMOVE_GOOGLE_BOOK
     };
 };
 
@@ -18,8 +25,11 @@ export default function(state = {}, action) {
     let newState = Object.assign({}, state);
 
     switch (action.type) {
-        case SET_BOOK:
+        case SET_GOOGLE_BOOK:
             newState = Object.assign({}, newState, action.book);
+            break;
+        case REMOVE_GOOGLE_BOOK:
+            newState = {};
             break;
         default:
             return state;
