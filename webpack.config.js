584b103e4e2e185dcf4ed16c783c723dae69dfc1
Elliott Brooks <ms.elliottbrooks@gmail.com>
2017-05-22 13:32:42 -0400

    modified webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,4 @@
-'use strict'
-
-const LiveReloadPlugin = require('webpack-livereload-plugin')
-    , devMode = require('.').isDevelopment
-
-/**
- * Fast source maps rebuild quickly during development, but only give a link
- * to the line where the error occurred. The stack trace will show the bundled
- * code, not the original code. Keep this on `false` for slower builds but
- * usable stack traces. Set to `true` if you want to speed up development.
- */
-
-    , USE_FAST_SOURCE_MAPS = false
+'use strict';
 
 module.exports = {
   entry: './browser/index.js',
@@ -19,9 +7,6 @@ module.exports = {
     filename: './public/bundle.js'
   },
   context: __dirname,
-  devtool: devMode && USE_FAST_SOURCE_MAPS
-    ? 'cheap-module-eval-source-map'
-    : 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '.json', '*']
   },
@@ -32,12 +17,9 @@ module.exports = {
       use: [{
         loader: 'babel-loader',
         options: {
-          presets: ['react', 'es2015', 'stage-2']
+          presets: ['react', 'es2015']
         }
       }]
     }]
-  },
-  plugins: devMode
-    ? [new LiveReloadPlugin({appendScriptTag: true})]
-    : []
+  }
 }
